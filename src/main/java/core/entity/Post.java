94acab6e0da6cdcdd510a82971a94782df443e39
tfiamietsh <ya.touka2017@yandex.ru>
2025-09-11 02:00:02 +0300

    Add post entity

---
+++ src/main/java/core/entity/Post.java
@@ -0,0 +1,49 @@
+package core.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "posts")
+@Data
+public class Post {
+    public enum Status {
+        PENDING,
+        PUBLISHED
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @Lob
+    private String text;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private User reviewer;
+}
