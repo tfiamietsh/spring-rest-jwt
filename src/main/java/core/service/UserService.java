cf0204228cc79d4fbee434e24cd7fd9de7050445
tfiamietsh <ya.touka2017@yandex.ru>
2025-09-10 04:51:10 +0300

    Minor fix user service

--- src/main/java/core/service/UserService.java
+++ src/main/java/core/service/UserService.java
@@ -20,7 +20,7 @@ public class UserService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
     }
