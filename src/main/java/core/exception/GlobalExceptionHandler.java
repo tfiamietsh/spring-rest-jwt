7e21934ac0143558ecddb661f3b3dfae49005d1a
tfiamietsh <ya.touka2017@yandex.ru>
2025-09-10 22:30:45 +0300

    Add global exception handler

---
+++ src/main/java/core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package core.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage).collect(Collectors.toList()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> badCredentialsExceptionHandler(BadCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getLocalizedMessage());
+    }
+}
