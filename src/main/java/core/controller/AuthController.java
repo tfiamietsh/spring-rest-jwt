195939e843131af2f603d819a10fd73487860d66
tfiamietsh <ya.touka2017@yandex.ru>
2025-09-11 01:14:29 +0300

    Add auth service

--- src/main/java/core/controller/AuthController.java
+++ src/main/java/core/controller/AuthController.java
@@ -1,6 +1,7 @@
 package core.controller;
 
 import core.entity.User;
+import core.service.AuthService;
 import core.service.JwtService;
 import core.service.UserService;
 
@@ -8,9 +9,6 @@ import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,13 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
+    private final AuthService authService;
     private final UserService userService;
     private final JwtService jwtService;
 
-    public AuthController(AuthenticationManager authenticationManager, UserService userService,
-                          JwtService jwtService) {
-        this.authenticationManager = authenticationManager;
+    public AuthController(AuthService authService, UserService userService, JwtService jwtService) {
+        this.authService = authService;
         this.userService = userService;
         this.jwtService = jwtService;
     }
@@ -46,11 +43,9 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<String> postLogin(@RequestBody User user) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
-        );
+        authService.authenticate(user);
 
-        User authenticatedUser = (User) authentication.getPrincipal();
+        User authenticatedUser = authService.getAuthenticatedUser();
         String jwt = jwtService.generateToken(authenticatedUser);
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(jwt);
---
+++ src/main/java/core/service/AuthService.java
@@ -0,0 +1,30 @@
+package core.service;
+
+import core.entity.User;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthService(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    public void authenticate(User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    public User getAuthenticatedUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
